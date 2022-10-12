5ec34c96bad09a344b4747263570eb1e1b2464a4
Orlandd <109672644+Orlandd@users.noreply.github.com>
2022-10-13 00:03:56 +0700

    Update uts11.c

--- uts11.c
+++ uts11.c
@@ -1,107 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
-void jumlah(double sales[5][6]) {
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%d\t%lf\t%lf\t%lf\t%lf\t%lf\t\n", i+1, sales[i][0], sales[i][1], sales[i][2], sales[i][3], sales[i][4]);
-    }
-}
-
-void data(int kode) {
-    double total = 0.0;
-    double sales[5][6];
-    int produk;
-    int kodeKaryawan;
-
-    if(kode == 1 ) {
-        printf("Produk ke- :");
-        scanf("%d", &produk);
-        produk -= 1;
-        printf("Penghasilan: ");
-        scanf("%lf", &sales[0][produk]);
-
-        printf("\n");
-        printf("Kode Karyawan Sales : ");
-        scanf("%d", &kodeKaryawan);
-        
-        data(kodeKaryawan);
-        // printf("%lf", sales[0][1]);
-    } else if (kode == 2) {
-        printf("Produk ke- :");
-        scanf("%d", &produk);
-        produk -= 1;
-        printf("Penghasilan: ");
-        scanf("%lf", &sales[1][produk]);
-
-        printf("\n");
-        printf("Kode Karyawan Sales : ");
-        scanf("%d", &kodeKaryawan);
-
-        data(kodeKaryawan);
-    } else if (kode == 3) {
-        printf("Produk ke- :");
-        scanf("%d", &produk);
-        produk -= 1;
-        printf("Penghasilan: ");
-        scanf("%lf", &sales[2][produk]);
-
-        printf("\n");
-        printf("Kode Karyawan Sales : ");
-        scanf("%d", &kodeKaryawan);
-
-        data(kodeKaryawan);
-    } else if (kode == 4) {
-        printf("Produk ke- :");
-        scanf("%d", &produk);
-        produk -= 1;
-        printf("Penghasilan: ");
-        scanf("%lf", &sales[3][produk]);
-
-        printf("\n");
-        printf("Kode Karyawan Sales : ");
-        scanf("%d", &kodeKaryawan);
-
-        data(kodeKaryawan);
-    } else if (kode == 5) {
-        printf("Produk ke- :");
-        scanf("%d", &produk);
-        produk -= 1;
-        printf("Penghasilan: ");
-        scanf("%lf", &sales[4][produk]);
-
-        printf("\n");
-        printf("Kode Karyawan Sales : ");
-        scanf("%d", &kodeKaryawan);
-
-        data(kodeKaryawan);
-    }
-
-}
-
-
-
+// Alven Henas Orlando
+// L0122016
+// UTS no 3.1
 void main() {
-    // const int ORANG = 5, PRODUKS = 6;
-    // double sales[ ORANG ][ PRODUKS ] , value, totalSales, productSales[ PRODUKS ] ;
-    // int salesPerson, product;
-    // scanf("%d", &salesPerson);
-    
-    // while (salesPerson != 0)
-    // {
-    //     scanf("%d%d", &product, &value);
-    //     printf("product : %d  value: %d", product, value);
-    //     sales[salesPerson][product] += value;
-    //     printf("%d", sales[1][2]);
-    // }
-    // printf("test");
 
     int orang = 5;
     int produk;
     double sales[5][6];
     int salesPeople;
-    // double value1, value2, value3, value4, value5, value6 = 0;
+
     double value[7] ={0.0};
     double valueX[7] ={0,0};
 
@@ -112,6 +21,7 @@ void main() {
     printf("Kode Karyawan Sales : ");
     scanf("%d", &salesPeople);
 
+    //pengambil input user (looping)
     while (salesPeople != 0) {
         printf("Produk ke- :");
         scanf("%d", &produk);
@@ -124,83 +34,35 @@ void main() {
 
     }
 
-    // for (int i = 0; i < 4; i++) {
-    //     for (int j = 0; i < 5; j++)
-    //     {
-    //         value[i] += sales[i][j];
-    //     }
-    // }
-
-    // for (int i = 0; i < 5 ; i++) {
-    //     for (int j = 0; i < 4; j++)
-    //     {
-    //         valueX[i] += sales[j][i];
-    //     }
-    // }
-
-    for (int i = 0; i < 5; i++) {
-        value[0] += sales[0][i];
-    }
-    for (int i = 0; i < 5; i++) {
-        value[1] += sales[1][i];
-    }
-    for (int i = 0; i < 5; i++) {
-        value[2] += sales[2][i];
-    }
-    for (int i = 0; i < 5; i++) {
-        value[3] += sales[3][i];
-    }
-
-
+    //perhitungan total
     for (int i = 0; i < 4; i++) {
-        valueX[0] += sales[i][0];
-    }
-    for (int i = 0; i < 4; i++) {
-        valueX[1] += sales[i][1];
-    }
-    for (int i = 0; i < 4; i++) {
-        valueX[2] += sales[i][2];
-    }
-    for (int i = 0; i < 4; i++) {
-        valueX[3] += sales[i][3];
-    }
-    for (int i = 0; i < 4; i++) {
-        valueX[4] += sales[i][4];
+        for (int j = 0; j < 5; j++)
+        {
+            value[i] += sales[i][j];
+        }
     }
 
-
-    // for (int i = 0; i < 4; i++) {
-    //     value[5] += value[i];
-    // }
+    for (int i = 0; i < 5 ; i++) {
+        for (int j = 0; j < 4; j++)
+        {
+            valueX[i] += sales[j][i];
+        }
+    }
 
     for (int i = 0; i < 4; i++) {
         valueX[6] += value[i];
     }
 
-
-    // value6 = value1+value2+value3+value4;
-
-    printf(" \t1\t\t2\t\t3\t\t4\t\t5\t\t\n");
+    //Menampilkan tabel 
+    system("cls");
+    printf(" \t1\t2\t3\t4\t5\ttotal\n");
     printf("======================================================================================================\n");
 
     for (int i = 0; i < 4; i++)
     {
-        printf("%d\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", i+1, sales[i][0], sales[i][1], sales[i][2], sales[i][3], sales[i][4], value[i]);
+        printf("%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", i+1, sales[i][0], sales[i][1], sales[i][2], sales[i][3], sales[i][4], value[i]);
     }
     printf("======================================================================================================\n");
-    printf("total\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", valueX[0], valueX[1], valueX[2], valueX[3], valueX[4], valueX[6]);
+    printf("total\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", valueX[0], valueX[1], valueX[2], valueX[3], valueX[4], valueX[6]);
 
-    // data(salesPeople);
-    // jumlah(sales);
-
-    // printf("Masukan data penjualan\n");
-
-    // for (int i = 0; i < 4; i++) {
-    //     for (int j = 0 ; j < 5 ; j++) {
-    //         printf("Sales no %d\n", i+1);
-    //         printf("Produk %d :", j+1);
-    //         scanf("%d", &sales[i][j]);
-    //         printf("\n");
-    //     }
-    // }
 }
