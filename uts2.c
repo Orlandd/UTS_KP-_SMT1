cbca27ca1ea7435af7eafa2cc22d144107780f37
Orlandd <109672644+Orlandd@users.noreply.github.com>
2022-10-13 00:04:49 +0700

    update soal 2

--- uts2.c
+++ uts2.c
@@ -1,37 +1,15 @@
 #include <stdio.h>
-
+// Alven Henas Orlando
+// L0122016
+// UTS no 3.2
 void main() {
+    // membuat array dengan index 0-999 yang value nya 1
     int angka[1000];
     for (int i = 0; i < 1000; i++) {
         angka[i] = 1;
     };
 
-    // int x = 2;
-
-    // while (x * x <= 1000)
-    // {
-    //     int n = 2;
-    //     while (n * n <= 1000)
-    //     {
-    //         angka[x*n] = 0;
-    //         n++;
-    //     }
-
-    //     while (angka[x] == 0)
-    //     {
-    //         x++;
-    //     }
-        
-        
-    // }
-
-    // for (int i = 0; i <= 1000; i++) {
-    //     int n = 2*i;
-    //     int m = 3;
-
-    //     angka[n] = 0;
-    // }
-
+    // perkalian untuk menentukan bilangan prima 
     for(int i = 2 ; i < 1000 ; i++){
         if(angka[i]) {
             for(int a = 2 ; a*i <= 1000 ;a++) {
@@ -40,9 +18,13 @@ void main() {
         }
     }
     
-    printf("Angka 3 = %d\n", angka[3]);
-    
-    for (int i = 1; i < 1000; i++) {
-        printf("Angka %d = %d\n", i, angka[i]);
+    printf("==========BILANGAN PRIMA==========\n");
+
+    //print bilangan prima (looping)
+    for (int i = 2; i < 1000; i++) {
+        if(angka[i] == 1 ){
+        printf("Angka %d \n", i );
+            
+        }
     };
 }
